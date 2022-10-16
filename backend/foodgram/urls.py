2678f32f04947086e5e7bf0a394a3c4aab69ac65
Vladimir Kirillov <kirill-vladimir@yandex.ru>
2022-10-16 11:23:43 +0300

    debug true

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#     )
